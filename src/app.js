e326db2802a09b7b77ff410f199ad5c09f68f1b7
Amit Suman <amit.suman456@gmail.com>
2024-12-21 16:15:36 +0530

    tested get, post,delete

--- src/app.js
+++ src/app.js
@@ -4,16 +4,20 @@ const app = express();
 
 //the below callback function is known as request handler, app.use is known as the middleware
 
-app.use("/hello", (req, res)=>{
-    res.send("Hello from the Hello")
+app.get("/user", (req, res)=>{
+    res.send({fisrtName: "Amit", lastName: "Suman"})
+})
+
+app.post("/user", (req, res)=>{
+    res.send("Data Successfully send to Database")
+})
+
+app.delete("/user", (req, res)=>{
+    res.send("Data Successfully deleted to Database")
 })
 
 app.use("/test", (req, res)=>{
     res.send("Hello from the test")
-})
-
-app.use("/", (req, res)=>{
-    res.send("Hello from the dashboard")
 })
 
 //app.listen takes port number and callback function as the parameter 
