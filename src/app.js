8cf457cd7dfedcc7dbbd86b9c19c94ff5e48ce20
Amit Suman <amit.suman456@gmail.com>
2024-12-25 23:19:30 +0530

    added bcrypt

--- src/app.js
+++ src/app.js
@@ -2,11 +2,19 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
   try {
+    //validation of the data
+    validateSignUpData(req);
+    //Encrpt the data
+    const {firstName, lastName, email, password} = req.body;
+    const passwordHash = await bcrypt.hash(password, 10)
+    console.log(passwordHash)
+    const user = new User({firstName, lastName, email, password:passwordHash});
     await user.save();
     res.send("User Added Successfully");
   } catch (err) {
@@ -55,20 +63,22 @@ app.patch("/user/:userId", async (req, res) => {
   const userId = req.params?.userId;
   const data = req.body;
   try {
-    const ALLOWED_UPDATES = [
-      "photoUrl","about","gender","age","skills"
-    ]
-    const isUpdateAllowed = Object.keys(data).every((k)=>ALLOWED_UPDATES.includes(k))
-    if(!isUpdateAllowed){
-      throw new Error("Update not allowed")
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
     }
-    if(data?.skills.length>10){
-      throw new Error("Skills cannot be more than 10")
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
     }
-    const updatedUser = await User.findByIdAndUpdate(userId, data,{runValidators: true});
-    res.send("User Updated Successfully"+updatedUser);
+    const updatedUser = await User.findByIdAndUpdate(userId, data, {
+      runValidators: true,
+    });
+    res.send("User Updated Successfully" + updatedUser);
   } catch (err) {
-    res.status(400).send("Something went Wrong"+err);
+    res.status(400).send("Something went Wrong" + err);
   }
 });
 
