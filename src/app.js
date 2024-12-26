7773f151863b4bc60ffbef5af0c1c4f1ee6c65c1
Amit Suman <amit.suman456@gmail.com>
2024-12-26 22:43:43 +0530

    cookie-parser and jwt

--- src/app.js
+++ src/app.js
@@ -4,17 +4,25 @@ const app = express();
 const User = require("./models/user");
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
-
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 app.use(express.json());
+app.use(cookieParser());
+
 app.post("/signup", async (req, res) => {
   try {
     //validation of the data
     validateSignUpData(req);
     //Encrpt the data
-    const {firstName, lastName, email, password} = req.body;
-    const passwordHash = await bcrypt.hash(password, 10)
-    console.log(passwordHash)
-    const user = new User({firstName, lastName, email, password:passwordHash});
+    const { firstName, lastName, email, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
     await user.save();
     res.send("User Added Successfully");
   } catch (err) {
@@ -23,23 +31,44 @@ app.post("/signup", async (req, res) => {
 });
 
 //login API
-app.post("/login", async (req,res)=>{
-  try{
-    const {email, password} = req.body;
-    const user = await User.findOne({email: email});
-    if(!user){
-      throw new Error("Email is not valid")
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      throw new Error("Email is not valid");
     }
-    const isPasswordValid = bcrypt.compare(password, user.password)
-    if(isPasswordValid)
-      res.send("Login Successful")
-    else
-      throw new Error("Password is not valid")
-  }
-  catch(err){
+    const isPasswordValid = bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      //create a JWT
+      const token = await jwt.sign({ _id: user._id }, "DEV@Tinder123");
+      //attach the token to the cookie and send the response back to the user
+      res.cookie("token", token);
+      res.send("Login Successful");
+    } else throw new Error("Password is not valid");
+  } catch (err) {
     res.status(400).send("Error saving the user: " + err.message);
   }
-})
+});
+
+//Profile of the user
+app.get("/profile", async (req, res) => {
+  try {
+    const cookies = req.cookies;
+
+    const { token } = cookies;
+    //validate the token
+    if (!token) throw new Error("Invalid Token");
+    const decoded = await jwt.verify(token, "DEV@Tinder123");
+    const { _id } = decoded;
+
+    const user = await User.findOne({ _id });
+    if (!user) throw new Error("User doesn't exist");
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
 //Get User by email
 app.get("/user", async (req, res) => {
   const userEmail = req.body.email;
